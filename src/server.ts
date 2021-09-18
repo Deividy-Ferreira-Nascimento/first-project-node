f6dbc0705a960c3fc20cf76f5fe9fb0bcccece8a
Deividy Ferreira <deividyferreira.n@gmail.com>
2021-09-18 03:48:54 -0300

    pause Aula

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import express from 'express';
+import routes from './routes';
 
 
 const app = express();
 
-app.get('/', (req,res) => {
-    return res.json({message: 'Hello GoStack'})
-})
+app.use(express.json());
+
+app.use(routes)
 
 app.listen(3333, () => {
     console.log('running ')
